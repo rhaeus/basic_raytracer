b14f90244ee507e16f5cf1b424a68cd12ad6c552
Ramona Häuselmann <ramona.haeuselmann@gmail.com>
2021-05-09 14:10:06 +0200

    fix bug in super sampling

--- src/Raytracer.cpp
+++ src/Raytracer.cpp
@@ -9,7 +9,7 @@
 Raytracer::Raytracer(int width_, int height_, Camera camera_, Scene scene_, int maxRecursions_)
     : width(width_), height(height_), camera(camera_), scene(scene_), maxRecursions(maxRecursions_)
 {
-    superSamples = 4;
+    superSamples = 16; // must be square number, casts that many rays per pixel
 }
 
 inline
@@ -60,14 +60,16 @@ glm::vec3 Raytracer::castRayAtPixel(int x, int y)
 
     float weight = 1.0f / superSamples;
 
-    int stop = sqrt(superSamples);
+    float samplesPerDimension = sqrt(superSamples);
+    float stepX = pixelWidth/samplesPerDimension;
+    float stepY = pixelHeight/samplesPerDimension;
 
     glm::vec3 color(0,0,0);
 
-    for (int c = 0; c < stop; ++c) {
-        for (int r = 0; r < stop; ++r) {
-            float sampleX = x_coord_left + (r+0.5) * pixelWidth;
-            float sampleY = y_coord_top + (c+0.5) * pixelHeight;
+    for (int c = 0; c < (int)samplesPerDimension; ++c) {
+        for (int r = 0; r < (int)samplesPerDimension; ++r) {
+            float sampleX = x_coord_left + (r+0.5) * stepX;
+            float sampleY = y_coord_top - (c+0.5) * stepY;
             
             glm::vec3 rayDir(sampleX, sampleY, 1);
             rayDir = glm::normalize(rayDir);
